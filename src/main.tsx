ae12dc8930a8e5b4df620791aec737f8eb571848
Johan-Taborda <johan.david.taborda@gmail.com>
2025-04-27 01:12:28 -0500

    Se usa react-bootstrap para el nuevo dropdown.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importación de estilos de Bootstrap
 import './index.css'
 import App from './App.tsx'
 
--- src/modules/layout/Header/Header.tsx
+++ src/modules/layout/Header/Header.tsx
@@ -1,28 +1,26 @@
 import { FaUser } from "react-icons/fa";
-import { useState } from "react";
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export const Header = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    
     const userIcon = () => (<FaUser className="text-2xl" />);
 
     return (
         <div className="bg-gray-500 w-full h-[80px] flex items-center justify-end pr-8">
-            <div className="relative">
-                <button 
-                    className="btn m-1"
-                    onClick={() => setIsOpen(!isOpen)}
+            <Dropdown align="end">
+                <Dropdown.Toggle 
+                    as="div" 
+                    id="dropdown-basic" 
+                    className="border-0 bg-transparent cursor-pointer"  // le quité variant
                 >
                     {userIcon()}
-                </button>
-                
-                {isOpen && (
-                    <ul className="absolute right-0 menu bg-base-100 rounded-box z-[1000] w-52 p-2 shadow-md mt-2">
-                        <li><a>Perfil</a></li>
-                        <li><a>Cerrar Sesión</a></li>
-                    </ul>
-                )}
-            </div>
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu className="mt-2 shadow-sm">
+                    <Dropdown.Item>Perfil</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item className="text-danger">Cerrar Sesión</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown> 
         </div>
     )
 }
